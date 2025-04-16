83c1046f1627c078417f3b666ddea164b54f365c
Makino0327 <k024g0008@g.neec.ac.jp>
2025-04-16 13:57:52 +0900

    a

--- DirectXGame/GameScene.h
+++ DirectXGame/GameScene.h
@@ -1,8 +1,14 @@
 #pragma once
 #include "KamataEngine.h"
 class GameScene {
+public:
 
-	public:
+	uint32_t textureHandle_ = 0;
+	KamataEngine::Sprite* sprite_ = nullptr;
+
+public:
+
+	~GameScene();
 
 	void Initialize();
 
